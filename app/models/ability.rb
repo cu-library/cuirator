91edcfc023d728d10b02a010a92a18b1cdd908e0
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2021-02-18 14:33:58 -0500

    Put custom model ability in the right place

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,12 @@ class Ability
   include Hyrax::Ability
   self.ability_logic += [:everyone_can_create_curation_concerns]
 
-  if current_user.admin?
-    can [:create, :show, :add_user, :remove_user, :index, :edit, :update, :destroy], Role
-  end
-
   # Define any customized permissions here.
   def custom_permissions
     # Limits deleting objects to a the admin user
+    if current_user.admin?
+      can [:create, :show, :add_user, :remove_user, :index, :edit, :update, :destroy], Role
+    end
     #
     # if current_user.admin?
     #   can [:destroy], ActiveFedora::Base
