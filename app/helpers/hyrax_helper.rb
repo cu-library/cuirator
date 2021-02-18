b6317c6cda89638bb236aac7b851da22979c8117
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2021-02-18 15:43:22 -0500

    First try at customizing the UI

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -3,4 +3,17 @@ module HyraxHelper
   include ::BlacklightHelper
   include Hyrax::BlacklightOverride
   include Hyrax::HyraxHelperBehavior
+
+  def application_name
+    'Digital Collections'
+  end
+
+  def institution_name
+    'Carleton University Library'
+  end
+
+  def  institution_name_full 
+    'Carleton University Library'
+  end
+
 end
