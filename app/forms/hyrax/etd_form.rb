b95f10b6e05c23a0707b69fcdd33fbe4352b10f2
Jen Whitney <jen.j.whitney@gmail.com>
2021-07-14 15:55:28 +0000

    Add degree fields to ETD form. Add resource type to required fields.

--- app/forms/hyrax/etd_form.rb
+++ app/forms/hyrax/etd_form.rb
@@ -4,6 +4,7 @@ module Hyrax
   # Generated form for Etd
   class EtdForm < Hyrax::Forms::WorkForm
     self.model_class = ::Etd
-    self.terms += [:resource_type]
-  end
+    self.terms += [:resource_type, :degree_level, :degree, :degree_discipline]
+    self.required_fields += [:resource_type]
+   end
 end
