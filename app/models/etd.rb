9b5a2bca14af33fcffbe0e622d24b364003c8710
Jen Whitney <jen.j.whitney@gmail.com>
2021-07-14 15:43:38 +0000

    Add degree level, name, and discipline fields to ETD model

--- app/models/etd.rb
+++ app/models/etd.rb
@@ -8,6 +8,19 @@ class Etd < ActiveFedora::Base
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
 
+  # @todo use better URIs, check other ETDMS sources
+  property :degree_level, predicate: ::RDF::URI.new("http://digital.library.carleton.ca/ns#degree_level"), multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :degree, predicate: ::RDF::URI.new("http://digital.library.carleton.ca/ns#degree"), multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :degree_discipline, predicate: ::RDF::URI.new("http://digital.library.carleton.ca/ns#degree_discipline"), multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
