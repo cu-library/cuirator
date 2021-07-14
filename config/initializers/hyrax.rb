c16a208dca47ae298fb8e334e5a947e3308e3da3
Jen Whitney <jen.j.whitney@gmail.com>
2021-07-14 15:18:59 +0000

    Generate ETD work type

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -2,6 +2,8 @@
 Hyrax.config do |config|
   # Injected via `rails g hyrax:work Work`
   config.register_curation_concern :work
+  # Injected via `rails g hyrax:work Etd`
+  config.register_curation_concern :etd
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
