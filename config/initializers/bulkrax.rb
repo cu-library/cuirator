99d8dbe3be13e4d674f860fd77b707648bb2ab51
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2021-09-22 15:01:22 -0400

    Updated dependencies, new bulkrax config file format, hyrax 3.1.0

--- config/initializers/bulkrax.rb
+++ config/initializers/bulkrax.rb
@@ -6,10 +6,6 @@ Bulkrax.setup do |config|
   #   { name: 'MODS - My Local MODS parser', class_name: 'Bulkrax::ModsXmlParser', partial: 'mods_fields' },
   # ]
 
-  # Field to use during import to identify if the Work or Collection already exists.
-  # Default is 'source'.
-  # config.system_identifier_field = 'source'
-
   # WorkType to use as the default if none is specified in the import
   # Default is the first returned by Hyrax.config.curation_concerns
   # config.default_work_type = MyWork
@@ -22,17 +18,6 @@ Bulkrax.setup do |config|
 
   # Server name for oai request header
   # config.server_name = 'my_server@name.com'
-
-  # Field_mapping for establishing the source_identifier
-  # This value IS NOT used for OAI, so setting the OAI Entries here will have no effect
-  # The mapping is supplied per Entry, provide the full class name as a string, eg. 'Bulkrax::CsvEntry'
-  # Example:
-  #   {
-  #     'Bulkrax::RdfEntry'  => 'http://opaquenamespace.org/ns/identifier',
-  #     'Bulkrax::CsvEntry'  => 'MyIdentifierField'
-  #   }
-  # The default value for CSV is 'source_identifier'
-  # config.source_identifier_field_mapping = { }
 
   # Field_mapping for establishing a parent-child relationship (FROM parent TO child)
   # This can be a Collection to Work, or Work to Work relationship
@@ -58,13 +43,6 @@ Bulkrax.setup do |config|
   #   config.field_mappings = {
   #     "Bulkrax::OaiDcParser" => { **individual field mappings go here*** }
   #   }
-
-  # Add to, or change existing mappings as follows
-  #   e.g. to exclude date
-  #   config.field_mappings["Bulkrax::OaiDcParser"]["date"] = { from: ["date"], excluded: true  }
-
-  # split: true => split multi-valued field values on semi-colon or pipe
-  # split: '\#' => split on character or regex
   config.field_mappings["Bulkrax::CsvParser"]["creator"]     = { from: ["creator"], split: true }
   config.field_mappings["Bulkrax::CsvParser"]["contributor"] = { from: ["contributor"], split: true }
   config.field_mappings["Bulkrax::CsvParser"]["creator"]     = { from: ["creator"], split: true }
@@ -73,10 +51,29 @@ Bulkrax.setup do |config|
   config.field_mappings["Bulkrax::CsvParser"]["language"]    = { from: ["language"], split: true }
   config.field_mappings["Bulkrax::CsvParser"]["identifier"]  = { from: ["identifier"], split: '\||\;' }
 
+  # Add to, or change existing mappings as follows
+  #   e.g. to exclude date
+  #   config.field_mappings["Bulkrax::OaiDcParser"]["date"] = { from: ["date"], excluded: true  }
+  #
+  # #   e.g. to add the required source_identifier field
+  #   #   config.field_mappings["Bulkrax::CsvParser"]["source_id"] = { from: ["old_source_id"], source_identifier: true  }
+  # If you want Bulkrax to fill in source_identifiers for you, see below
+
   # To duplicate a set of mappings from one parser to another
   #   config.field_mappings["Bulkrax::OaiOmekaParser"] = {}
   #   config.field_mappings["Bulkrax::OaiDcParser"].each {|key,value| config.field_mappings["Bulkrax::OaiOmekaParser"][key] = value }
 
+  # Should Bulkrax make up source identifiers for you? This allow round tripping
+  # and download errored entries to still work, but does mean if you upload the
+  # same source record in two different files you WILL get duplicates.
+  # It is given two aruguments, self at the time of call and the index of the reocrd
+  #    config.fill_in_blank_source_identifiers = ->(parser, index) { "b-#{parser.importer.id}-#{index}"}
+  # or use a uuid
+  #    config.fill_in_blank_source_identifiers = ->(parser, index) { SecureRandom.uuid }
+
   # Properties that should not be used in imports/exports. They are reserved for use by Hyrax.
   # config.reserved_properties += ['my_field']
 end
+
+# Sidebar for hyrax 3+ support
+Hyrax::DashboardController.sidebar_partials[:repository_content] << "hyrax/dashboard/sidebar/bulkrax_sidebar_additions" if Object.const_defined?(:Hyrax) && ::Hyrax::DashboardController&.respond_to?(:sidebar_partials)
