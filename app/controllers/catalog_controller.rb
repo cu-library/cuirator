02294af868708388c79b3a6b956143123d90ca80
Jen Whitney <jen.j.whitney@gmail.com>
2021-10-14 20:27:30 +0000

    Remove Featured Works / Collections until more content is available. Update controller to provide Type facet label

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -67,8 +67,9 @@ class CatalogController < ApplicationController
     config.add_facet_field "file_format_sim", limit: 5
     config.add_facet_field "member_of_collection_ids_ssim", limit: 5, label: 'Collections', helper_method: :collection_title_by_id
 
-    # The generic_type and depositor are not displayed on the facet list
+    # The type, generic_type and depositor are not displayed on the facet list
     # They are used to give a label to the filters that comes from the user profile
+    config.add_facet_field "human_readable_type_sim", label: "Type", if: false
     config.add_facet_field "generic_type_sim", if: false
     config.add_facet_field "depositor_ssim", label: "Depositor", if: false
 
