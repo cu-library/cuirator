dfa17b227120b66a989234f3f6a5cce6ee369036
Jen Whitney <jen.j.whitney@gmail.com>
2021-11-03 15:53:26 +0000

    Update requirement for rights statement on ETD work type and add edit form hint for Rights Notes

--- app/forms/hyrax/etd_form.rb
+++ app/forms/hyrax/etd_form.rb
@@ -5,6 +5,7 @@ module Hyrax
   class EtdForm < Hyrax::Forms::WorkForm
     self.model_class = ::Etd
     self.terms += [:resource_type, :degree_level, :degree, :degree_discipline]
+    self.required_fields -= [:rights_statement]
     self.required_fields += [:resource_type, :degree_level, :degree, :degree_discipline]
    end
 end
