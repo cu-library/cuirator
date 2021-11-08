94086627672c9a38e2e84c7e75b32f3ab5ef5104
Jen Whitney <jen.j.whitney@gmail.com>
2021-11-08 16:39:40 +0000

    Remove unused index view helper to facet language

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -26,18 +26,9 @@ module HyraxHelper
     link_to_facet_term(value, label, 'degree_level_sim')
   end
 
-  def language_facet(options)
-    link_to_facet_term_list(options[:value], "language_sim")
-  end
-
   def link_to_facet_term(value, label, field)
     path = main_app.search_catalog_path(search_state.add_facet_params_and_redirect(field, value))
     link_to(label, path)
-  end
-
-  def link_to_facet_term_list(values, field, empty_message = "", separator = ", ")
-    return empty_message if values.blank?
-    safe_join(values.map { |value| link_to_facet_term(value, ::LanguagesService.label(value), field) }, separator)
   end
 
   def contributor_search(options)
