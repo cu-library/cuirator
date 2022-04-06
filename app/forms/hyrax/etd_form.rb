53c0ce65c4854d8d2e681d94e740d996a4005dcf
Jen Whitney <jen.j.whitney@gmail.com>
2022-04-06 12:24:02 -0400

    DI-149 - use source field for Bulkrax import/export: drop field from Work & ETD edit forms

--- app/forms/hyrax/etd_form.rb
+++ app/forms/hyrax/etd_form.rb
@@ -5,6 +5,8 @@ module Hyrax
   class EtdForm < Hyrax::Forms::WorkForm
     self.model_class = ::Etd
     self.terms += [:resource_type, :degree_level, :degree, :degree_discipline, :internal_note, :agreement]
+    # Bulkrax uses the source field to store identifiers used in import/export
+    self.terms -= [:source]
     self.required_fields -= [:rights_statement]
     self.required_fields += [:resource_type, :degree_level, :degree, :degree_discipline, :agreement]
    end
--- app/forms/hyrax/work_form.rb
+++ app/forms/hyrax/work_form.rb
@@ -5,6 +5,8 @@ module Hyrax
   class WorkForm < Hyrax::Forms::WorkForm
     self.model_class = ::Work
     self.terms += [:resource_type, :bibliographic_citation]
+    # Bulkrax uses the source field to store identifiers used in import/export
+    self.terms -= [:source]
     self.required_fields = [:title, :creator, :resource_type]
   end
 end
