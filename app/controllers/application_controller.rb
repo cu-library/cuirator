46c9a6f09cb5ee47c7b9f643c00fd76ad88a93d0
Manfred Raffelsieper <manfred@digitallibrary-manfreddev1.cloud.library.carleton.ca>
2022-05-17 16:14:55 -0400

    404 and 500 page fix and fileset fix included

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,4 @@ class ApplicationController < ActionController::Base
   include Hyrax::ThemedLayoutController
   with_themed_layout '1_column'
 
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  rescue_from Exception, with: :not_found
-  rescue_from ActionController::RoutingError, with: :not_found
-
 end
--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,18 +1,7 @@
 class ErrorsController < ApplicationController
-
-
+  
   def not_found
-    respond_to do |format|
-      format.html { render status: 404 }
-      format.json { render json: { message: "Not Found" }, status: 404 }
-      format.js   { render json: { message: "Not Found" }, status: 404 }
-    end
+    render status: 404
   end
 
-  def internal_server_error
-    respond_to do |format|
-      format.html { render status: 500 }
-      format.json { render json: { message: "Internal server error" }, status: 500 }
-    end
-  end
 end
