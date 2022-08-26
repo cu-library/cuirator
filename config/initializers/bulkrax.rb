220dc573c9e11a1173b5cfb2e8be92faa74be002
Manfred Raffelsieper <manfred@digitallibrary-manfreddev1.cloud.library.carleton.ca>
2022-08-26 09:46:19 -0400

    DI-207 removing duplicate config line

--- config/initializers/bulkrax.rb
+++ config/initializers/bulkrax.rb
@@ -40,7 +40,6 @@ Bulkrax.setup do |config|
   config.field_mappings["Bulkrax::CsvParser"]["subject"]     = { from: ["subject"], split: true }
   config.field_mappings["Bulkrax::CsvParser"]["language"]    = { from: ["language"], split: true }
   config.field_mappings["Bulkrax::CsvParser"]["identifier"]  = { from: ["identifier"], split: '\||\;' }
-  config.field_mappings["Bulkrax::CsvParser"]["agreements"]  = { from: ["agreements"], split: '\|\;' }
   config.field_mappings["Bulkrax::CsvParser"]["agreements"]  = { from: ["agreements"], split: true }
 
   # Add to, or change existing mappings as follows
