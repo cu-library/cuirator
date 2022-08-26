f35f70f1ffd8a6d0651adfdf060c80c8fd165be2
Jen Whitney <jen.j.whitney@gmail.com>
2022-08-26 10:11:30 -0400

    DI-208 Tell Solr where to find citation to display on ResearchWork

--- app/presenters/hyrax/research_work_presenter.rb
+++ app/presenters/hyrax/research_work_presenter.rb
@@ -2,5 +2,6 @@
 #  `rails generate hyrax:work ResearchWork`
 module Hyrax
   class ResearchWorkPresenter < Hyrax::WorkShowPresenter
+    delegate :bibliographic_citation, to: :solr_document
   end
 end
