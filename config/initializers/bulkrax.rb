dc5a41a66a4cb9cf8f3db6d7c2d686724e9df8b6
Manfred Raffelsieper <manfred@digitallibrary-manfreddev1.cloud.library.carleton.ca>
2022-10-05 11:35:10 -0400

    DI-124 Pushing updated bulkrax.config

--- config/initializers/bulkrax.rb
+++ config/initializers/bulkrax.rb
@@ -40,7 +40,7 @@ Bulkrax.setup do |config|
   config.field_mappings["Bulkrax::CsvParser"]["subject"]     = { from: ["subject"], split: true }
   config.field_mappings["Bulkrax::CsvParser"]["language"]    = { from: ["language"], split: true }
   config.field_mappings["Bulkrax::CsvParser"]["identifier"]  = { from: ["identifier"], split: '\||\;' }
-  config.field_mappings["Bulkrax::CsvParser"]["agreements"]  = { from: ["agreements"], split: true }
+  config.field_mappings["Bulkrax::CsvParser"]["agreement"]  = { from: ["agreement"], split: '\||\;' }
   config.field_mappings["Bulkrax::CsvParser"]["publisher"]   = { from: ["publisher"], split: true }
   config.field_mappings["Bulkrax::CsvParser"]["remote_files"] = { from: ["remote_files"], split: '\||\;' }
 
