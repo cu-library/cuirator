58f86b6288b238415b387a9e23be5cfa441b134e
Jen Whitney <jen.j.whitney@gmail.com>
2023-03-06 16:29:17 -0500

    Add custom date formats for DI-316

---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,8 @@
+# Custom date formats, e.g.,
+#
+#   Date.parse(value).to_formatted_s(:custom_format)
+#
+# to parse a string value as date and apply format
+#
+# Year for Date Created facet and ETD Date Created display
+Date::DATE_FORMATS[:year] = '%Y'
