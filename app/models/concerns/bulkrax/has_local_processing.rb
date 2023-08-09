91e2ea60b4702d489606cff05f01a04037b32245
Jen Whitney <jen.j.whitney@gmail.com>
2023-08-09 17:39:22 -0400

    DI-428 - Bulkrax local processing: restore line breaks removed from CSV import data

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -6,36 +6,65 @@ module Bulkrax::HasLocalProcessing
   # to add a custom property from outside of the import data
   def add_local
     add_geonames
+    restore_linebreaks
   end
 
-  def add_geonames
-    # get import file column name & split pattern from Bulkrax config
-    unless Bulkrax.field_mappings["Bulkrax::CsvParser"]["based_near"].nil?
-      geonames_field = Bulkrax.field_mappings["Bulkrax::CsvParser"]["based_near"][:from]&.first.to_s
-      geonames_split = Bulkrax.field_mappings["Bulkrax::CsvParser"]["based_near"][:split]&.to_s
-    end
-
-    # set defaults if not mapped in config
-    geonames_field ||= "based_near"
-    geonames_split ||= "[|]{3}"
-
-    if raw_metadata.has_key?(geonames_field)
-      geonames_entries = raw_metadata[geonames_field].to_s.split(Regexp.new(geonames_split))
-
-      # remove values parsed into work's based_near attribute
-      parsed_metadata.delete("based_near")
-
-      # ... add based_near_attributes expected in create, update
-      parsed_metadata["based_near_attributes"] ||= {}
-
-      # filter out anything that's not a URI
-      geonames_uris = geonames_entries.map { |entry| sanitize_uri(entry) }.compact
-
-      # add remaining URIs to based_near_attributes
-      geonames_uris.each_with_index do |value, i|
-        parsed_metadata["based_near_attributes"][i] = { "id" => value }
+    # Use Geonames URIs to create a based_near_attributes hash for creating, 
+    # updating Hyrax::ControlledVocabulary::Location attributes
+    def add_geonames
+      # get import file column name & split pattern from Bulkrax config
+      source_field = get_source_field("based_near")
+      
+      if source_field and raw_metadata.has_key?(source_field)
+        geonames_entries = raw_metadata[source_field].to_s.split(Regexp.new(get_split_pattern(source_field)))
+  
+        # remove values parsed into work's based_near attribute
+        parsed_metadata.delete("based_near")
+  
+        # ... add based_near_attributes expected in create, update
+        parsed_metadata["based_near_attributes"] ||= {}
+  
+        # filter out anything that's not a URI
+        geonames_uris = geonames_entries.map { |entry| sanitize_uri(entry) }.compact
+  
+        # add remaining URIs to based_near_attributes
+        geonames_uris.each_with_index do |value, i|
+          parsed_metadata["based_near_attributes"][i] = { "id" => value }
+        end
       end
     end
+
+  # Import data may contain line breaks in block text fields for readability, in
+  # attributes where the order of text blocks matters, e.g., description, abstract
+  # Bulkrax removes line breaks when processing import data at 
+  #   https://github.com/samvera-labs/bulkrax/blob/659887008387db29930239bb9f98f66a5845e14f/app/matchers/bulkrax/application_matcher.rb#L28
+  # For now, restore line breaks from raw metadata
+  def restore_linebreaks
+    # Fields with displayed with local renderer that retains line breaks
+    [ "description", "abstract", "rights_notes", "access_right", "internal_note" ].each do |field|
+      source_field = get_source_field(field)
+      next unless source_field
+
+      if raw_metadata.has_key?(source_field)
+        parsed_metadata[field] = raw_metadata[source_field].to_s.split(Regexp.new(get_split_pattern(field)))
+      end
+    end
+  end
+
+  def get_source_field(field) 
+    # get import file column name Bulkrax config
+    unless Bulkrax.field_mappings["Bulkrax::CsvParser"][field].nil?
+      return Bulkrax.field_mappings["Bulkrax::CsvParser"][field][:from]&.first.to_s
+    end
+  end
+
+  def get_split_pattern(field)
+    # get field split pattern from Bulkrax config or use default
+    unless Bulkrax.field_mappings["Bulkrax::CsvParser"][field].nil?
+      split_pattern =  Bulkrax.field_mappings["Bulkrax::CsvParser"][field][:split]&.to_s
+    end
+
+    split_pattern ||= "[|]{3}"
   end
 
   def sanitize_uri(value)
