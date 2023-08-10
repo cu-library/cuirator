c5e998c95b2a8a7a278bdd108192e105e5dfe2dd
Jen Whitney <jen.j.whitney@gmail.com>
2023-08-10 16:36:41 -0400

    DI-430 more flexible parsing in year-date renderer

--- app/renderers/year_date_attribute_renderer.rb
+++ app/renderers/year_date_attribute_renderer.rb
@@ -1,9 +1,18 @@
+# app/renderers/year_date_attribute_renderer.rb
 
+# Etds store YYYY-MM-DD submission date in Date Created
+# Format for display as YYYY to correspond w/ date on title page
 class YearDateAttributeRenderer < Hyrax::Renderers::DateAttributeRenderer
-      
+
   def attribute_value_to_html(value)
-    # @todo add date parsing
-    value[0,4]
+    # YYYY-MM-DD format date is expected, other full-date formats may be provided
+    year_date = Date.parse(value).year.to_s
+
+    # If not, try a YYYY format date
+    year_date ||= Date.strptime(value, "%Y").year.to_s
+  rescue
+    # Unknown format. Display value as entered so it's visible for clean-up
+    value
   end
   
 end
--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,7 @@ module Cuirator
     config.load_defaults 5.2
 
     config.exceptions_app = self.routes
-    config.active_record.yaml_column_permitted_classes = [Date]
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
