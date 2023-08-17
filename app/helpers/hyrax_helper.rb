30946e13ec70ff6417f05ecf3d1bc4537636c3be
Jen Whitney <jen.j.whitney@gmail.com>
2023-08-17 13:42:03 -0400

    DI-434 Better date parsing in local indexers

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -59,4 +59,19 @@ module HyraxHelper
     ::LanguagesService.label(value)
   end
 
+  # Indexing helpers
+
+  # Used in Work, Research Work, and Etd indexers to parse a
+  # YYYY-format date for Date Created facet and in Etd display
+  def date_created_year(value)
+    # YYYY-MM-DD format date is expected, other full-date formats may be provided
+    year_date = Date.parse(value).year.to_s
+
+    # If not, try a YYYY format date
+    year_date ||= Date.strptime(value, "%Y").year.to_s
+  rescue
+    # Unknown format. Display value as entered so it's visible for clean-up
+    value
+  end
+
 end
