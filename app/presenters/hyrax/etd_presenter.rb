6ac80ef56919a6eb7e03860fab7838285fb41c80
Jen Whitney <jen.j.whitney@gmail.com>
2023-10-18 15:27:41 -0400

    Cherry-pick commit from main branch: filter admin-only properties from JSON response for work show pages

--- app/presenters/hyrax/etd_presenter.rb
+++ app/presenters/hyrax/etd_presenter.rb
@@ -3,5 +3,15 @@
 module Hyrax
   class EtdPresenter < Hyrax::WorkShowPresenter
     delegate :degree_level, :degree, :degree_discipline, :internal_note, :agreement, :date_created_year, to: :solr_document
+
+    def json_admin_properties
+      #  Admin-only properties to filter from JSON response
+      json_properties = %i[
+        agreement
+        internal_note
+      ]
+      current_ability.can?(:write, id) ? [] : json_properties
+    end
+    
   end
 end
--- app/presenters/hyrax/research_work_presenter.rb
+++ app/presenters/hyrax/research_work_presenter.rb
@@ -3,5 +3,12 @@
 module Hyrax
   class ResearchWorkPresenter < Hyrax::WorkShowPresenter
     delegate :bibliographic_citation, :date_created_year, to: :solr_document
+
+    def json_admin_properties
+      # Admin-only properties to filter from ResearchWork JSON 
+      json_properties = %i[
+      ]
+    end
+    
   end
 end
