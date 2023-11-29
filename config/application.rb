9b2ae712f476c0fa516badea1a24ddbf8638bd5f
Manfred Raffelsieper <manfredraffelsieper@cunet.carleton.ca>
2023-11-29 16:34:47 -0500

    DI-387 Setting locals in config

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Cuirator
 
     config.exceptions_app = self.routes
 
+    config.i18n.available_locales = [:en]
+    config.i18n.default_locale = :en
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
