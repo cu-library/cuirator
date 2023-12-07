b6c570e2974434cac8f0cbc7dbed99d3991eb08a
Jen Whitney <jen.j.whitney@gmail.com>
2023-12-07 10:49:21 -0500

    WIP: create_work feature spec -- log in through UI directly

--- spec/features/create_work_spec.rb
+++ spec/features/create_work_spec.rb
@@ -1,30 +1,13 @@
 require 'rails_helper'
-include Warden::Test::Helpers
+# include Warden::Test::Helpers
 
 RSpec.feature 'Create a work', js: true do
 
   context 'as an admin user' do
-    # find admin user seeded in db
+    # attributes for admin user seeded in db
     let(:user_attributes) do
-      { email: 'admin@example.com' }
+      { email: 'admin@example.com', password: 'admin_password' }
     end
-
-    # find the user
-    let(:user) do
-      User.find_by(user_attributes)
-    end
-
-    # find or create default admin set
-    let(:admin_set_id) { AdminSet.find_or_create_default_admin_set_id }
-
-    # Get default admin set permissions 
-    let(:permission_template) { Hyrax::PermissionTemplate.find_or_create_by!(source_id: admin_set_id) }
-
-    # Use 'default' workflow for admin set (vs mediated deposit, or some other defined workflow)
-    let(:workflow) { Sipity::Workflow.find_by!(name: 'default', permission_template: permission_template) }
-
-    # Sipity::Agent is a proxy for an entity that can take an action -- user, group, etc
-    let(:user_agent) { Sipity::Agent.where(proxy_for_id: user.id, proxy_for_type: 'User').first_or_create }
 
     # Rquired metadata
     # Assign a unique title, to run / find subsequent tests
@@ -36,32 +19,27 @@ RSpec.feature 'Create a work', js: true do
     let(:date_created) { "2023-11-30" }
     let(:abstract) { "An abstract is a brief summary of the work." }
 
-    before do
-      #  Hyrax::PermissionTemplateAccess models a single grant of access to an agent (user or group) on a PermissionTemplate
-      Hyrax::PermissionTemplateAccess.create(
-        permission_template: permission_template,
-        agent_type: 'user',
-        agent_id: user.user_key,
-        access: 'deposit')
-
-      # Assigns depositor role to deposit workflow for user agent
-      Hyrax::Workflow::PermissionGenerator.call(roles: 'depositing', workflow: workflow, agents: user_agent)
-
-      # Log in as admin user
-      login_as user
-    end
-
     scenario do
-      # Navigate dashboard UI to create a new work
-      visit '/concern/works/new'
-    
+      # Navigate to login page
+      visit '/users/sign_in'
+      expect(page).to have_content "Log in"
+
       # Clear the cookie notice & confirm it's no longer visible
       # Otherwise, the new work's save button is not available to receive a click
       expect(page).to have_content "This site uses cookies"
       click_button "Ok. Got it."
       expect(page).not_to have_content "This site uses cookies"
 
-      # Confirm user has option to create a new work
+      # Fill in username & password
+      fill_in("Email", with: user_attributes[:email] )      
+      fill_in("Password", with: user_attributes[:password])
+      click_on("Log in")
+
+      # Confirm logged-in user has view of Dashboard
+      expect(page).to have_content "Dashboard"
+
+      # Visit page to create a new work
+      visit '/concern/works/new'
       expect(page).to have_content "Add New Work"
 
       # Switch to Files tab
@@ -113,7 +91,6 @@ RSpec.feature 'Create a work', js: true do
       expect(page).to have_content date_created
       expect(page).to have_content abstract
 
-      # Save screenshots to help debug tests -- see spec/support/capybara.rb
       save_screenshot
     end
   end
