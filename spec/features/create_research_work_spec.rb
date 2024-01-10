03e540f88db2532b2c0ab89087bf5169222d5475
Jen Whitney <jen.j.whitney@gmail.com>
2024-01-10 10:24:15 -0500

    Add feature spec: create ResearchWork

--- spec/features/create_research_work_spec.rb
+++ spec/features/create_research_work_spec.rb
@@ -1,14 +1,130 @@
 # Generated via
 #  `rails generate hyrax:work ResearchWork`
 require 'rails_helper'
-include Warden::Test::Helpers
 
-# NOTE: If you generated more than one work, you have to set "js: true"
-RSpec.feature 'Create a ResearchWork', js: false do
-  context 'a logged in user' do
+RSpec.feature 'Create a ResearchWork', js: true do
 
-    it "has tests" do
-      skip "Add your tests here"
+  # Rquired metadata
+  let(:work_title) { 'Create a generic work ' + Time.new.strftime("%Y-%m-%d %H:%M:%S") }
+  let(:creator) { 'Surname, Given Name' }
+  let(:resource_type) { 'Article' }
+
+    # Optional metadata
+  let(:abstract) { 'An abstract is a brief summary of the work.' }
+  let(:keywords) { ['Keyword', 'Descriptive phrase', 'Research area'] }
+  let(:license) { 'Creative Commons BY Attribution 4.0 International' }
+  let(:rights_notes) { 'Copyright © 2023 the author(s)' }
+  let(:publisher) { 'Research Journal Publisher' }
+  let(:date_created) { '2023-11-30' }
+  let(:language) { 'English' }
+  let(:identifier) { 'DOI: https://doi.org/10.22215/1234' }
+  let(:citation) { "Surname, G. (2023). #{work_title}. #{publisher}. https://doi.org/10.22215/1234" }
+
+  # Context for Library staff user only - create_work_spec.rb confirms
+  # deposit by admin user and no access to deposit works by basic user
+  context 'as a Library staff user' do
+
+    # attributes for staff user seeded in db
+    let(:user_attributes) { {email: 'staff_user@example.com', password: 'staff_password'} }
+
+    scenario do
+      # Navigate to login page
+      visit '/users/sign_in'
+      expect(page).to have_content 'Log in'
+
+      # Clear the cookie notice & confirm it's no longer visible
+      # Otherwise, the new work's save button is not available to receive a click
+      expect(page).to have_content 'This site uses cookies'
+      click_button 'Ok. Got it.'
+      expect(page).not_to have_content 'This site uses cookies'
+
+      # Fill in username & password
+      fill_in('Email', with: user_attributes[:email] )      
+      fill_in('Password', with: user_attributes[:password])
+      click_on('Log in')
+
+      # Confirm logged-in user has view of Dashboard
+      expect(page).to have_content 'Dashboard'
+
+      # Visit page to create a new research work
+      click_link 'Works'
+      expect(page).to have_content 'Add New Work'
+
+      # Choose Research Work type
+      click_on 'Add New Work'
+      choose 'payload_concern', option: 'ResearchWork'
+      click_button 'Create work'
+      expect(page).to have_content 'Add New Research Work'
+
+      # Switch to Files tab
+      click_link 'Files'
+      expect(page).to have_content 'Add files'
+
+      # Upload file(s) 
+      within('div#add-files') do
+        attach_file('files[]', File.join(fixture_path, 'research_work.pdf'), visible: false)
+      end
+
+      # Add required metadata
+      click_link 'Descriptions'
+      fill_in('Title', with: work_title)
+      fill_in('Creator', with: creator)
+      select(resource_type, from: 'Resource type')
+
+      # With Selenium / Chromedriver, focus remains on the select box. Click
+      # body to move focus outside select box so next element can be found.
+      find('body').click
+    
+      # Add optional metadata
+      click_on 'Additional fields'
+      fill_in('Abstract', with: abstract)
+    
+      # Set keyword, then click 'Add another' for each additional entry
+      keywords.each do |keyword|
+        page.all('input.research_work_keyword').last.set(keyword)
+        click_on('Add another Keyword') unless keyword == keywords.last
+      end
+
+      select license, from: 'License'
+      fill_in('Rights notes', with: rights_notes)
+      fill_in('Publisher', with: publisher)
+      fill_in('Date Created', with: date_created)
+      select language, from: 'Language'
+      fill_in('Identifier', with: identifier)
+      fill_in('Citation', with: citation)
+
+      # Set work visibility
+      choose('research_work_visibility_open')
+
+      # Accept deposit agreement
+      check('agreement')
+
+      click_on("Save")
+
+      # Expect work files to be processing in the background
+      expect(page).to have_content "Your files are being processed"
+  
+      # Required metadata
+      expect(page).to have_content work_title
+      expect(page).to have_content creator
+      expect(page).to have_content resource_type
+  
+      # Optional metadata
+      expect(page).to have_content abstract
+      keywords.each { |keyword| expect(page).to have_content keyword }
+      expect(page).to have_content license
+      expect(page).to have_content rights_notes
+      expect(page).to have_content publisher
+      expect(page).to have_content date_created
+      expect(page).to have_content language
+      expect(page).to have_content identifier 
+      expect(page).to have_content citation
+
+      # Log out user
+      visit '/users/sign_out'
+      expect(page).to have_content "Signed out successfully"
     end
+
   end
+
 end
