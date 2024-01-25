ae4fda2f8b4552cb2be2e41af5f337f5368ee057
Jen Whitney <jen.j.whitney@gmail.com>
2024-01-25 15:19:12 -0500

    DI-451 specs: query catalog JSON by Bulkax source identifier

--- spec/features/browse_catalog_spec.rb
+++ spec/features/browse_catalog_spec.rb
@@ -1,19 +1,32 @@
 require 'rails_helper'
 
 RSpec.feature 'Browse the catalog', js: true do
+  let(:etd) { FactoryBot.create(:public_etd) }
+
+  # Bulkrax requires a source identifier
+  let(:bulkrax_etd) { FactoryBot.create(:public_etd, source: ['abcd-efgh-ijkl-mnop']) } 
 
   context 'when not logged in' do
+    scenario 'limited-access Etd fields are NOT included in RDF' do
+      # Filter catalog by Etd identifer
+      visit '/catalog.jsonld?f[id][]=' + etd.id
 
-    scenario 'limited-access Etd fields are not included in RDF' do
-      FactoryBot.create(:public_etd)
+      # parse as JSON
+      etd_jsonld = JSON.parse(page.text)
+      expect(etd_jsonld.keys.include?('bibo:Note')).to be false
+      expect(etd_jsonld.keys.include?('schema:license')).to be false
+    end
 
-      # Browse Etds
-      visit '/catalog.jsonld?f[human_readable_type_sim][]=Etd'
+    scenario 'catalog JSON can be queried by Bulkrax source identifier' do
+      visit '/catalog.json?f[source_tesim][]=' + bulkrax_etd.source.first
 
-      # JSON-LD may not parse correctly. Check page content agreement, internal_note predicates
-      expect(page).not_to have_content 'bibo:Note'
-      expect(page).not_to have_content 'schema:license'
+      # Expect Etd id to be present in JSON
+      etd_json = JSON.parse(page.text)['data'].first
+      expect(etd_json.keys.include?('id')).to be true
+      expect(etd_json['id']).to eq bulkrax_etd.id
     end
   end
+
+
 
 end
