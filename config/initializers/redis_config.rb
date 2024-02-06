c6b658444d704baa624a8c38e16a589053d58d88
Jen Whitney <jen.j.whitney@gmail.com>
2024-02-06 13:42:09 -0500

    DI-452 - fix type in redis_config.rb

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -8,4 +8,4 @@ config[:thread_safe] = true
 size = ENV.fetch("HYRAX_REDIS_POOL_SIZE", 5)
 timeout = ENV.fetch("HYRAX_REDIS_TIMEOUT", 5)
 
-Hyrax.config.redis_connection = ConnectionPool::Wrapper.new(size: size, teimout: timeout) { Redis.new(config) }
+Hyrax.config.redis_connection = ConnectionPool::Wrapper.new(size: size, timeout: timeout) { Redis.new(config) }
