28ddca5b16679daa2efdff159de1f3adba5fb0a0
Manfred Raffelsieper <manfredraffelsieper@cunet.carleton.ca>
2024-05-07 20:19:46 -0400

    DI-495 added internal note to both works & research works

--- app/forms/hyrax/work_form.rb
+++ app/forms/hyrax/work_form.rb
@@ -4,7 +4,7 @@ module Hyrax
   # Generated form for Work
   class WorkForm < Hyrax::Forms::WorkForm
     self.model_class = ::Work
-    self.terms += [:resource_type]
+    self.terms += [:resource_type, :internal_note]
     # Bulkrax uses the source field to store identifiers used in import/export
     self.terms -= [:source]
     self.required_fields = [:title, :resource_type]
