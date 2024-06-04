34c10a5d50fa0e8e2e634e4c6091afbdef34b260
Jen Whitney <jen.j.whitney@gmail.com>
2024-06-04 15:36:05 -0400

    DI-51 oai_etdms spec & Etd factory updates

--- spec/factories/etds.rb
+++ spec/factories/etds.rb
@@ -1,19 +1,36 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-
   factory :etd do
     # Required metadata
-    title { ['Thesis or dissertation title ' + Time.new.strftime("%Y-%m-%d %H:%M:%S")] }
+    title { ["Thesis or dissertation title #{Time.new.strftime('%Y-%m-%d %H:%M:%S')}"] }
     creator { ['Surname, Given Name'] }
     resource_type { ['Thesis'] }
     degree_level { '1' } # Master's degree
     degree { 'Master of Science (M.Sc.)' }
     degree_discipline { 'Engineering' }
 
+    # Optional metadata commonly provided for Etds
+    contributor { ['Person, First Name (Thesis advisor)'] }
+    subject { ['Subject Area', 'Subject Area -- Ontario', 'Subject Area -- 20th century'] }
+    abstract { ['A summary of the work.'] }
+    publisher { ['Carleton University'] }
+    date_created { ['2022-04-10'] }
+    identifier { ['https://doi.org/10.22215/2023-12345'] }
+    language { ['eng'] }
+    rights_notes { ['Copyright (c) 2022 the author.'] }
+
     # Optional, limited-access metadata (admin, Library staff only)
     internal_note { ['This is an internal note added by the Metadata team.'] }
-    agreement { ['https://repository.library.carleton.ca/concern/works/pc289j04q'] } # Carleton University Thesis Licence Agreement
+
+    # Carleton University Thesis Licence Agreement and LAC licence
+    # See config/authorities.yml
+    agreement do
+      [
+        'https://repository.library.carleton.ca/concern/works/pc289j04q',
+        'https://repository.library.carleton.ca/concern/works/6h440t871'
+      ]
+    end
 
     transient do
       depositing_user { User.find_by(email: 'staff_user@example.com') }
@@ -29,10 +46,32 @@ FactoryBot.define do
       visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
     end
 
-    trait :private do 
+    trait :private do
       # default visibility is private
     end
 
+    # Public ETD with public file, licenced to Carleton and LAC
+    factory :public_etd_with_public_file, traits: [:public] do
+      before(:create) do |etd, context|
+        etd.ordered_members << create(:public_file)
+      end
+    end
+
+    # Public ETD with private file, licenced to Carleton and LAC
+    factory :public_etd_with_private_file, traits: [:public] do
+      before(:create) do |etd, context|
+        etd.ordered_members << create(:private_file)
+      end
+    end
+
+    # Private ETD with private file, licenced to Carleton and LAC
+    factory :private_etd_with_private_file, traits: [:private] do
+      before(:create) do |etd, context|
+        etd.ordered_members << create(:private_file)
+      end
+    end
+
+    # Public ETD with CU & LAC licences, but no files
     factory :public_etd, traits: [:public]
   end
 end
--- spec/requests/oai_pmh_endpoint_spec.rb
+++ spec/requests/oai_pmh_endpoint_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'OAI-PMH endpoint' do
@@ -31,11 +32,10 @@ RSpec.describe 'OAI-PMH endpoint' do
   end
 
   describe 'GetRecord verb' do
-
     context 'for a Work' do
       let(:work_attributes) do
         {
-          title: ['Example work in OAI-DC record'  + Time.new.strftime('%Y-%m-%d %H:%M:%S')],
+          title: ["Example work in OAI-DC record#{Time.new.strftime('%Y-%m-%d %H:%M:%S')}"],
           creator: ['Lastname, Given'],
           contributor: ['Surname, First'],
           date_created: ['2023-04-02'],
@@ -60,66 +60,66 @@ RSpec.describe 'OAI-PMH endpoint' do
       end
 
       it 'includes work URL in dc:identifier' do
-        expect(response.body).to include('<dc:identifier>' + repository_url.gsub('/catalog/oai', '/concern/works/' + work.id) + '</dc:identifier>')
+        expect(response.body).to include("<dc:identifier>#{repository_url.gsub('/catalog/oai', "/concern/works/#{work.id}")}</dc:identifier>")
       end
 
       it 'includes Date Created in dc:date with YYYY-MM-DD format' do
-        expect(response.body).to include('<dc:date>' + work.date_created.first + '</dc:date>')
+        expect(response.body).to include("<dc:date>#{work.date_created.first}</dc:date>")
       end
 
       it 'includes Title in dc:title' do
-        expect(response.body).to include('<dc:title>' + work.title.first + '</dc:title>')
+        expect(response.body).to include("<dc:title>#{work.title.first}</dc:title>")
       end
 
       it 'includes Creator in dc:creator' do
-        expect(response.body).to include('<dc:creator>' + work.creator.first + '</dc:creator>')
+        expect(response.body).to include("<dc:creator>#{work.creator.first}</dc:creator>")
       end
 
       it 'includes Contributor in dc:contributor' do
-        expect(response.body).to include('<dc:contributor>' + work.contributor.first + '</dc:contributor>')
+        expect(response.body).to include("<dc:contributor>#{work.contributor.first}</dc:contributor>")
       end
 
       it 'includes Rights Statement, Rights Notes, and Creative Commons license in dc:rights' do
-        expect(response.body).to include('<dc:rights>' + work.rights_statement.first + '</dc:rights>')
-        expect(response.body).to include('<dc:rights>' + work.rights_notes.first + '</dc:rights>')
-        expect(response.body).to include('<dc:rights>' + work.license.first + '</dc:rights>')
+        expect(response.body).to include("<dc:rights>#{work.rights_statement.first}</dc:rights>")
+        expect(response.body).to include("<dc:rights>#{work.rights_notes.first}</dc:rights>")
+        expect(response.body).to include("<dc:rights>#{work.license.first}</dc:rights>")
       end
 
       it 'includes Publisher in dc:publisher' do
-        expect(response.body).to include('<dc:publisher>' + work.publisher.first + '</dc:publisher>')
+        expect(response.body).to include("<dc:publisher>#{work.publisher.first}</dc:publisher>")
       end
 
       it 'includes Identifier in dc:identifier' do
-        expect(response.body).to include('<dc:identifier>' + work.identifier.first + '</dc:identifier>')
+        expect(response.body).to include("<dc:identifier>#{work.identifier.first}</dc:identifier>")
       end
 
       it 'includes Language in dc:language' do
-        expect(response.body).to include('<dc:language>' + work.language.first + '</dc:language>')
+        expect(response.body).to include("<dc:language>#{work.language.first}</dc:language>")
       end
 
       it 'includes Resource Type in dc:type' do
-        expect(response.body).to include('<dc:type>' + work.resource_type.first + '</dc:type>')
+        expect(response.body).to include("<dc:type>#{work.resource_type.first}</dc:type>")
       end
 
       it 'includes Keyword and Subject in dc:subject' do
-        work.keyword.each { |keyword| expect(response.body).to include('<dc:subject>' + keyword + '</dc:subject>')}
-        work.subject.each { |subject| expect(response.body).to include('<dc:subject>' + subject + '</dc:subject>')}
+        work.keyword.each { |keyword| expect(response.body).to include("<dc:subject>#{keyword}</dc:subject>")}
+        work.subject.each { |subject| expect(response.body).to include("<dc:subject>#{subject}</dc:subject>")}
       end
-      
+
       it 'includes Abstract and Description in dc:description' do
-        expect(response.body).to include('<dc:description>' + work.abstract.first + '</dc:description>')
-        expect(response.body).to include('<dc:description>' + work.description.first + '</dc:description>')
+        expect(response.body).to include("<dc:description>#{work.abstract.first}</dc:description>")
+        expect(response.body).to include("<dc:description>#{work.description.first}</dc:description>")
       end
 
       it 'includes Related URL in dc:relation' do
-        expect(response.body).to include('<dc:relation>' + work.related_url.first + '</dc:relation>')
+        expect(response.body).to include("<dc:relation>#{work.related_url.first}</dc:relation>")
       end
     end
 
     context 'for an Etd' do
       let(:etd_attributes) do
         {
-          title: ['Example Etd in OAI-DC record'  + Time.new.strftime('%Y-%m-%d %H:%M:%S')],
+          title: ["Example Etd in OAI-DC record#{Time.new.strftime('%Y-%m-%d %H:%M:%S')}"],
           creator: ['Lastname, Given'],
           contributor: ['Surname, First (Supervisor)'],
           date_created: ['2023-04-02'],
@@ -139,51 +139,51 @@ RSpec.describe 'OAI-PMH endpoint' do
       end
 
       it 'includes Etd URL in dc:identifier' do
-        expect(response.body).to include('<dc:identifier>' + repository_url.gsub('/catalog/oai', '/concern/etds/' + etd.id) + '</dc:identifier>')
+        expect(response.body).to include("<dc:identifier>#{repository_url.gsub('/catalog/oai', "/concern/etds/#{etd.id}")}</dc:identifier>")
       end
 
       it 'includes Date Created in dc:date with YYYY format' do
-        expect(response.body).to include('<dc:date>' + Date.parse(etd.date_created.first).year.to_s + '</dc:date>')
+        expect(response.body).to include("<dc:date>#{Date.parse(etd.date_created.first).year.to_s}</dc:date>")
       end
 
       it 'includes Title in dc:title' do
-        expect(response.body).to include('<dc:title>' + etd.title.first + '</dc:title>')
+        expect(response.body).to include("<dc:title>#{etd.title.first}</dc:title>")
       end
 
       it 'includes Creator in dc:creator' do
-        expect(response.body).to include('<dc:creator>' + etd.creator.first + '</dc:creator>')
+        expect(response.body).to include("<dc:creator>#{etd.creator.first}</dc:creator>")
       end
 
       it 'includes Contributor in dc:contributor' do
-        expect(response.body).to include('<dc:contributor>' + etd.contributor.first + '</dc:contributor>')
+        expect(response.body).to include("<dc:contributor>#{etd.contributor.first}</dc:contributor>")
       end
 
       it 'includes Rights Notes in dc:rights' do
-        expect(response.body).to include('<dc:rights>' + etd.rights_notes.first + '</dc:rights>')
+        expect(response.body).to include("<dc:rights>#{etd.rights_notes.first}</dc:rights>")
       end
 
       it 'includes Publisher in dc:publisher' do
-        expect(response.body).to include('<dc:publisher>' + etd.publisher.first + '</dc:publisher>')
+        expect(response.body).to include("<dc:publisher>#{etd.publisher.first}</dc:publisher>")
       end
 
       it 'includes Identifier in dc:identifier' do
-        expect(response.body).to include('<dc:identifier>' + etd.identifier.first + '</dc:identifier>')
+        expect(response.body).to include("<dc:identifier>#{etd.identifier.first}</dc:identifier>")
       end
 
       it 'includes Language in dc:language' do
-        expect(response.body).to include('<dc:language>' + etd.language.first + '</dc:language>')
+        expect(response.body).to include("<dc:language>#{etd.language.first}</dc:language>")
       end
 
       it 'includes Resource Type in dc:type' do
-        expect(response.body).to include('<dc:type>' + etd.resource_type.first + '</dc:type>')
+        expect(response.body).to include("<dc:type>#{etd.resource_type.first}</dc:type>")
       end
 
       it 'includes Subject in dc:subject' do
-        etd.subject.each { |subject| expect(response.body).to include('<dc:subject>' + subject + '</dc:subject>')}
+        etd.subject.each { |subject| expect(response.body).to include("<dc:subject>#{subject}</dc:subject>")}
       end
-      
+
       it 'includes Abstract in dc:description' do
-        expect(response.body).to include('<dc:description>' + etd.abstract.first + '</dc:description>')
+        expect(response.body).to include("<dc:description>#{etd.abstract.first}</dc:description>")
       end
     end
   end
@@ -192,9 +192,9 @@ RSpec.describe 'OAI-PMH endpoint' do
   #   it 'displays Collections as sets'
   #   ... etc
   # Factory / factories to create collection type, collection & add works to confirm OAI ListSets config, etc.,
-  # is a lot of overhead that would duplicate efforts in Hyrax. See https://github.com/samvera/hyrax/pull/6664 
+  # is a lot of overhead that would duplicate efforts in Hyrax. See https://github.com/samvera/hyrax/pull/6664
   # and e.g. spec/factories/collections.rb. Being able to use upstream factories will be great when we upgrade
-  # to Hyrax v5! For now, fetch OAI config & confirm Sets are configured based on collection membership. 
+  # to Hyrax v5! For now, fetch OAI config & confirm Sets are configured based on collection membership.
   describe 'ListSets verb' do
     it 'is configured to show Collections as Sets' do
       expect(CatalogController.blacklight_config.oai[:document][:set_fields].count).to eq(1)
@@ -202,5 +202,4 @@ RSpec.describe 'OAI-PMH endpoint' do
       expect(CatalogController.blacklight_config.oai[:document][:set_fields][0][:solr_field]).to eq('member_of_collections_ssim')
     end
   end
-
 end
